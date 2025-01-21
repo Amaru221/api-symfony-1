29446c696e73f83b64163e41b23e2d9d8e0209f4
null221 <agd1923@gmail.com>
2025-01-21 17:49:14 +0100

    edited method defaults with defined values

--- src/Factory/DragonTreasureFactory.php
+++ src/Factory/DragonTreasureFactory.php
@@ -29,6 +29,20 @@ use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
  * @method static DragonTreasure[]|Proxy[] randomSet(int $number, array $attributes = [])
  */
 final class DragonTreasureFactory extends PersistentProxyObjectFactory{
+    
+    private const TREASURE_NAMES = ['pile of gold coins', 'diamond-encrusted
+    throne', 'rare magic staff', 'enchanted sword', 'set of intricately crafted
+    goblets', 'collection of ancient tomes', 'hoard of shiny gemstones', 'chest
+    filled with priceless works of art', 'giant pearl', 'crown made of pure
+    platinum', 'giant egg (possibly a dragon egg?)', 'set of ornate armor', 'set of
+    golden utensils', 'statue carved from a single block of marble', 'collection of
+    rare, antique weapons', 'box of rare, exotic chocolates', 'set of ornate
+    jewelry', 'set of rare, antique books', 'giant ball of yarn', 'life-sized statue
+    of the dragon itself', 'collection of old, used toothbrushes', 'box of mismatched
+    socks', 'set of outdated electronics (such as CRT TVs or floppy disks)', 'giant
+    jar of pickles', 'collection of novelty mugs with silly sayings', 'pile of old
+    board games', 'giant slinky', 'collection of rare, exotic hats'];
+    
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
      *
@@ -48,15 +62,15 @@ final class DragonTreasureFactory extends PersistentProxyObjectFactory{
      *
      * @todo add your default values here
      */
-    protected function defaults(): array|callable
+    protected function defaults(): array
     {
         return [
-            'coolFactor' => self::faker()->randomNumber(),
-            'description' => self::faker()->text(),
-            'isPublished' => self::faker()->boolean(),
-            'name' => self::faker()->text(255),
-            'plunderedAt' => \DateTimeImmutable::createFromMutable(self::faker()->dateTime()),
-            'value' => self::faker()->randomNumber(),
+        'coolFactor' => self::faker()->numberBetween(1, 10),
+        'description' => self::faker()->paragraph(),
+        'isPublished' => self::faker()->boolean(),
+        'name' => self::faker()->randomElement(self::TREASURE_NAMES),
+        'plunderedAt' => \DateTimeImmutable::createFromMutable(self::faker()->dateTimeBetween('-1 year')),
+        'value' => self::faker()->numberBetween(1000, 1000000),
         ];
     }
 
