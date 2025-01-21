7b8a78610cf550eaa8198e046a816abb3ebf3dab
null221 <agd1923@gmail.com>
2025-01-21 18:22:09 +0100

    edited load method, creating 40 objects

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\DragonTreasure;
+use App\Factory\DragonTreasureFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        DragonTreasureFactory::createMany(40);
     }
 }
