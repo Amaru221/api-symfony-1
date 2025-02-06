980a2c44ec3fd29fea825681405e8243dd4c3202
null221 <agd1923@gmail.com>
2025-02-06 19:18:43 +0100

    added createMany 10 users in load method AppFixtures class

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,17 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UserFactory;
 use App\Entity\DragonTreasure;
 use App\Factory\DragonTreasureFactory;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         DragonTreasureFactory::createMany(40);
+        UserFactory::createMany(10);
     }
 }
