dc3d055d93248c5098e6a5ef929d4bfd1b72ac73
null221 <agd1923@gmail.com>
2025-03-04 11:04:47 +0100

    replaced API URL by props

--- assets/react/controllers/ReactAdmin.jsx
+++ assets/react/controllers/ReactAdmin.jsx
@@ -3,4 +3,4 @@ import React from 'react';
 
 // Replace with your own API entrypoint
 // For instance if https://example.com/api/books is the path to the collection of book resources, then the entrypoint is https://example.com/api
-export default () => <HydraAdmin entrypoint="https://demo.api-platform.com" />;
+export default (props) => <HydraAdmin entrypoint={props.entrypoint} />;
