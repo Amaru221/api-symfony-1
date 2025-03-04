2d6e1cfdbf279dd1a4c3b6e96f052f20773f8497
null221 <agd1923@gmail.com>
2025-03-04 10:58:04 +0100

    created First Controller. ReactAdmin

---
+++ assets/react/controllers/ReactAdmin.jsx
@@ -0,0 +1,6 @@
+import { HydraAdmin } from '@api-platform/admin';
+import React from 'react';
+
+// Replace with your own API entrypoint
+// For instance if https://example.com/api/books is the path to the collection of book resources, then the entrypoint is https://example.com/api
+export default () => <HydraAdmin entrypoint="https://demo.api-platform.com" />;
